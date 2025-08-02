96ec7fd0655f8195717eedeb8302021e54f0948e
Mariana Lira <85141413+marianaliraf@users.noreply.github.com>
2025-08-02 15:38:25 -0300

    Update handler.py
    Inserção variavel de ambiente PORT

--- health_insurance/handler.py
+++ health_insurance/handler.py
@@ -51,4 +51,5 @@ def healthinsurance_predict():
         return Response( '{}', status=200, mimetype='application/json' )
     
 if __name__ == '__main__':
-    app.run( '0.0.0.0', debug=True )
+    port = os.environ.get('PORT', 5000)
+    app.run(host='0.0.0.0', port=port, debug=True)
